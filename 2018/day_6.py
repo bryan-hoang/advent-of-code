70c65a95a37e285e89543aa6ec7feee91aa32680
Bryan Hoang <bryan.c.hoang@gmail.com>
2019-12-02 10:57:33 -0500

    Finish 2018 Day 6 Part 2

--- 2018/day_6.py
+++ 2018/day_6.py
@@ -1,31 +1,36 @@
-def create_map_with_bounding_box(coordinates) -> list:
+def create_voronoi_map_with_bounding_box(input_coordinates):
+    map_height, map_width, max_x_coordinate, max_y_coordinate, min_x_coordinate, min_y_coordinate = get_map_dimensions(
+        input_coordinates)
+
+    map_of_points = [['.'] * map_height for _ in range(map_width)]
+    number_of_points_in_safe_region = 0
+    for y in range(min_y_coordinate, max_y_coordinate):
+        for x in range(min_x_coordinate, max_x_coordinate):
+            region, is_safe_region = assign_point_to_region((x, y), input_coordinates)
+            number_of_points_in_safe_region += 1 if is_safe_region else 0
+            map_of_points[x - min_x_coordinate][max_y_coordinate - 1 - y] = region
+
+    return map_of_points, number_of_points_in_safe_region
+
+
+def get_map_dimensions(coordinates):
     min_x_coordinate = min(coordinates, key=lambda coordinate: (coordinate[1][0]))[1][0]
     min_y_coordinate = min(coordinates, key=lambda coordinate: (coordinate[1][1]))[1][1]
     max_y_coordinate = max(coordinates, key=lambda coordinate: (coordinate[1][0]))[1][0]
     max_x_coordinate = max(coordinates, key=lambda coordinate: (coordinate[1][1]))[1][1]
     # print(min_x_coordinate, min_y_coordinate, max_x_coordinate, max_y_coordinate)
-
     map_width = max_x_coordinate - min_x_coordinate
     map_height = max_y_coordinate - min_y_coordinate
-
-    map_of_points = [['.'] * map_height for _ in range(map_width)]
-
-    for y in range(min_y_coordinate, max_y_coordinate):
-        for x in range(min_x_coordinate, max_x_coordinate):
-            region = assign_point_to_region((x, y), coordinates)
-            map_of_points[x - min_x_coordinate][max_y_coordinate - 1 - y] = region
-
-    return map_of_points
+    return map_height, map_width, max_x_coordinate, max_y_coordinate, min_x_coordinate, min_y_coordinate
 
 
-def assign_point_to_region(point: tuple, coordinates: list) -> str:
+def assign_point_to_region(point: tuple, coordinates: list):
     distances = dict(
         (given_coordinate[0], manhattan_distance(given_coordinate[1], point)) for given_coordinate in coordinates)
     min_distance = min(distances.values())
     closest_points = [p[0] for p in distances.items() if p[1] == min_distance]
-    if len(closest_points) == 1:
-        return closest_points[0]
-    return '.'
+    is_safe_region = sum(distances.values()) < 10000
+    return closest_points[0] if len(closest_points) == 1 else '.', is_safe_region
 
 
 def manhattan_distance(first_point: tuple, second_point: tuple) -> int:
@@ -54,6 +59,10 @@ def calculate_areas_of_each_region(map_of_points: list, infinite_regions: set) -
     return region_areas
 
 
+def calculate_safe_coordinates():
+    pass
+
+
 def print_map(map_to_print):
     for y in range(len(map_to_print[0])):
         line = ''
@@ -70,14 +79,15 @@ def main():
         coordinates[c] = next(coordinate_generator)
     # print(coordinates)
     # Part 1:
-    map_of_points = create_map_with_bounding_box(coordinates.items())
+    map_of_points, number_of_points_in_safe_region = create_voronoi_map_with_bounding_box(coordinates.items())
     print_map(map_of_points)
     infinite_regions = find_infinite_regions(map_of_points)
     # print(infinite_regions)
-    areas_of_each_region = calculate_areas_of_each_region(map_of_points, infinite_regions)
-    print(areas_of_each_region)
-    max_area = max(areas_of_each_region.values())
+    region_areas = calculate_areas_of_each_region(map_of_points, infinite_regions)
+    # print(region_areas)
+    max_area = max(region_areas.values())
     print(max_area)
+    print(number_of_points_in_safe_region)
 
 
 LABELS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwx'
